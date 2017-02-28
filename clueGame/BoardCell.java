caeb3cfb77a2adb5ef2327c942ec8df546b94f34
Sam Nichols <sanichol@mines.edu>
2017-02-28 11:30:14 -0700

    Added files

---
+++ clueGame/BoardCell.java
@@ -0,0 +1,16 @@
+package clueGame;
+
+public class BoardCell {
+	private int row;
+	private int column;
+
+	public BoardCell(){
+		
+	}
+	
+	public BoardCell(int r, int c) {
+		row = r;
+		column = c;
+	}
+
+}
